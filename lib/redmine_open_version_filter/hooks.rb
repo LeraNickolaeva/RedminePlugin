7db7387959e5e58dfb3613b28acb4adc1f7c69ec
LeraNickolaeva <leranick29@gmail.com>
2017-10-03 15:03:12 +0300

    Fix dropdown with "From Version" filter

--- lib/redmine_open_version_filter/hooks.rb
+++ lib/redmine_open_version_filter/hooks.rb
@@ -2,7 +2,7 @@ module RedmineOpenVersionFilter
   class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context={})
       if context[:controller] &&
-        (context[:controller].is_a?(IssuesController) || context[:controller].is_a?(QueriesController))
+        (context[:controller].is_a?(IssuesController) || context[:controller].is_a?(QueriesController) || context[:controller].is_a?(TimelogController)) 
         javascript_include_tag('open_version_filter.js', :plugin => 'redmine_open_version_filter')
       end
     end
